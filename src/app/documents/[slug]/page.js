489393218429fe23e7cc56bf1770df7d41343865
Oleksandr Rudenko <o.rudenko@olearis.com>
2025-03-08 16:53:13 +0000

    fix

--- src/app/documents/[slug]/page.js
+++ src/app/documents/[slug]/page.js
@@ -1,37 +1,36 @@
 // src/app/documents/[slug]/page.js
-// import { prisma } from "../../../db/db"; // Подключаем Prisma
-// import { notFound } from "next/navigation"; // Для возврата 404, если документ не найден
+import { prisma } from "../../../db/db"; // Подключаем Prisma
+import { notFound } from "next/navigation"; // Для возврата 404, если документ не найден
 
 // Асинхронный компонент для страницы документа
 export default async function DocumentPage() {
-  return <h1>DocumentPage</h1>;
-  // try {
-  //   const { slug } = await params; // Получаем слаг из URL
-  //   console.log("SERvR Log", slug);
+  try {
+    const { slug } = await params; // Получаем слаг из URL
+    console.log("SERvR Log", slug);
 
-  //   if (!slug) {
-  //     <h1>Ошибка сервера no slug</h1>;
-  //   }
+    if (!slug) {
+      <h1>Ошибка сервера no slug</h1>;
+    }
 
-  //   // Запрашиваем документ по слагу из базы данных
-  //   const document = await prisma.document.findUnique({
-  //     where: { slug },
-  //   });
+    // Запрашиваем документ по слагу из базы данных
+    const document = await prisma.document.findUnique({
+      where: { slug },
+    });
 
-  //   // Если документ не найден, возвращаем 404
-  //   if (!document) {
-  //     notFound(); // Это вызывает страницу 404
-  //   }
+    // Если документ не найден, возвращаем 404
+    if (!document) {
+      notFound(); // Это вызывает страницу 404
+    }
 
-  //   // Отображаем данные документа
-  //   return (
-  //     <div>
-  //       <h1>{document.title}</h1>
-  //       <p>{document.content}</p>
-  //     </div>
-  //   );
-  // } catch (error) {
-  //   console.error("Ошибка при загрузке документа:", error);
-  //   return <h1>Ошибка сервера</h1>;
-  // }
+    // Отображаем данные документа
+    return (
+      <div>
+        <h1>{document.title}</h1>
+        <p>{document.content}</p>
+      </div>
+    );
+  } catch (error) {
+    console.error("Ошибка при загрузке документа:", error);
+    return <h1>Ошибка сервера</h1>;
+  }
 }
